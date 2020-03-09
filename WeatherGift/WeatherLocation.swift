1c1fc2abbc9c621ce9092fe1008dd74b9edd3104
STEVEN ZHANG <zhangbsr@bc.edu>
2020-03-08 20:54:28 -0400

    7.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+ //
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by 张泽华 on 2020/3/8.
+//  Copyright © 2020 张泽华. All rights reserved.
+//
+
+import UIKit
+ 
+ struct WeatherLocation {
+    var name: String
+    var latituide: Double
+    var longitutde: Double
+ }
