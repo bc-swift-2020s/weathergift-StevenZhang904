7e348efad35bb3c7c7220d1e69c9416beb1fc495
STEVEN ZHANG <zhangbsr@bc.edu>
2020-03-18 20:15:22 -0400

    with .gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by 张泽华 on 2020/3/18.
+//  Copyright © 2020 张泽华. All rights reserved.
+//
+
+import Foundation
+struct APIkeys{
+    static let googlePlacesKey = "AIzaSyBLsyRilbuh8AOK2CG8TtJ9SlRP6RHP6hU"
+    
+}
